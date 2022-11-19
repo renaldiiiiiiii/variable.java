22c17c62bf9a6d80e89befb04522c98d636dd0a7
renaldiiiiiiii <115854896+renaldiiiiiiii@users.noreply.github.com>
2022-11-19 09:56:42 +0700

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,12 @@
+# variable.java
+public class Uts {
+	public static void main(String[] args) {
+		String var1 = "assalamu'alaikum";
+		String var2 = "Nama Saya : M Irgi Mutahari";
+		int tahun = 2002;
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println("Saya lahir Di Tahun : " + tahun);
+	}
+
+}
